320347f91ff361dcec802cbc00d255b9e7f309a3
Alexander Mychlo <alexmmych@gmail.com>
2020-05-13 17:57:41 +0100

    Re-did what was done because of a reason explained in the prev commit.

--- Compile.bat
+++ Compile.bat
@@ -1,7 +1,5 @@
 
-SET "sourcedir=C:\GameProject\CppFiles\"
-SET "librarydir=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\"
-SET "includedir=C:\Program Files (x86)\Windows Kits\10\Include\10.0.18362.0\"
+SET "sourcedir=C:\Optimade\CppFiles\"
 
 cd %sourcedir%
 
@@ -18,22 +16,9 @@ SET "CommandLinker=-L"
 SET "CommandLink=-l"
 SET "CommandInclude=-I"
 
-SET "target=\x64"
-
 FOR /f "delims=" %%v IN ('dir /a-d /b "%sourcedir%*.cpp"') DO (
 SET CppList=!CppList!%space%%%v
 )
 
-FOR /f "tokens=*" %%a IN ('dir /ad /b "%librarydir%*"') DO (
-SET LibList=!LibList!%space%%CommandLinker%%quote%!librarydir!%%a%target%%quote%
-)
-
-FOR /f "tokens=*" %%b IN ('dir /ad /b "%includedir%*"') DO (
-IF NOT [%%b] == [ucrt] (
-SET IncludeList=!IncludeList!%space%%CommandInclude%%quote%!includedir!%%b%quote%
-ECHO !IncludeList!
-)
-)
-
-g++%CppList% -std=c++17 -mwindows -o C:\GameProject\Game.exe
+g++%CppList% -std=c++17 -mwindows -o C:\Optimade\Optimade.exe
 PAUSE
--- HeaderFiles/pch.h
+++ HeaderFiles/pch.h
@@ -2,16 +2,7 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers.
 #endif
 
-#define _AMD64_ 1
-
-#include <sdkddkver.h>
 #include <windows.h>
-
-#include <wrl.h>
-#include <shellapi.h>
-
-#include "../HeaderFiles/d3dx12.h"
-#include <d3d12.h>
 
 #include <fstream>
 #include <iostream>
@@ -19,8 +10,3 @@
 #include <regex>
 #include <vector>
 #include <string>
-
-#include <d3dcompiler.h> //Allows to compile MHLSL (Microsoft High Level Shader Language) into bytecode.
-#include <dxgi1_6.h>  //Latest DXGI header.
-
-#include <D3Dcompiler.h>
